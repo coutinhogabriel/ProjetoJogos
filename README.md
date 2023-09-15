101bffca757b880e263e58930535bb1d3036a64c
coutinhogabriel <gabriel.coutin@hotmail.com>
2023-09-15 15:07:56 -0300

    Finalização piano virtual

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# ProjetoJogos
+Relatório do Projeto: Piano Virtual
+
+Introdução:
+O projeto consiste em um piano virtual interativo, que permite aos usuários tocar notas musicais pressionando as teclas do teclado ou clicando nas teclas na interface gráfica. Ele oferece a funcionalidade de alternar entre a exibição dos nomes das teclas e ocultá-los com um interruptor (checkbox), bem como um design estilizado para criar a aparência de um piano.
+
+Tecnologias Utilizadas:
+
+HTML: A estrutura básica da página web.
+CSS: Para a estilização e layout da interface do usuário.
+JavaScript: Para a lógica de interatividade do piano.
+Áudios (formato WAV): Para reproduzir as notas musicais do piano.
+Funcionalidades:
+
+Reprodução de Notas: O projeto permite aos usuários tocar notas musicais pressionando as teclas do teclado (como Tab, 1, q, 2, w, etc.) ou clicando nas teclas visuais na interface do piano. Cada tecla reproduz o som correspondente a uma nota musical.
+
+Alternância de Exibição: O projeto inclui um interruptor (checkbox) que permite alternar entre a exibição dos nomes das teclas e a ocultação dos nomes. Isso facilita o uso para iniciantes e permite que os músicos mais experientes ocultem as dicas visuais.
+
+Design Estilizado: A interface gráfica foi estilizada para criar uma aparência de piano realista. Isso inclui teclas brancas e pretas, bem como uma caixa de piano com bordas arredondadas.
+
+Sons Autênticos: O projeto incorpora arquivos de áudio (formato WAV) para cada nota musical, proporcionando uma experiência sonora autêntica ao tocar o piano virtual.
+
+Lógica do JavaScript:
+
+A reprodução das notas é controlada por funções playNote(), que usam a classe Audio para tocar arquivos de áudio correspondentes a cada nota.
+
+As teclas são configuradas para responder a eventos de clique do mouse e eventos de pressionamento de tecla usando event listeners. As teclas pretas e brancas têm suas próprias funções de tratamento de eventos.
+
+O interruptor (checkbox) controla a exibição ou ocultação dos nomes das teclas. Os nomes das teclas são controlados diretamente no JavaScript, alterando a propriedade de opacidade dos elementos <span> dentro das teclas.
+
+Estilização com CSS:
+
+O CSS é responsável por dar estilo à interface do piano. Ele define as cores, tamanhos, bordas, margens e posições de todos os elementos, incluindo as teclas do piano e o interruptor.
+
+As teclas do piano são estilizadas com cores brancas e pretas, bordas arredondadas e sombras para criar uma aparência autêntica.
+
+Conclusão:
+
+O projeto do piano virtual é uma implementação interativa e estilizada de um piano musical. Ele oferece funcionalidades para tocar notas musicais, alternar a exibição dos nomes das teclas e tem um design que se assemelha a um piano real. Este projeto pode ser usado como uma ferramenta educacional para aprender a tocar músicas no piano ou simplesmente para diversão e entretenimento musical.
--- script.js
+++ script.js
@@ -1,50 +1,91 @@
+// Seleciona todas as teclas do piano, a caixa de verificação, o interruptor e a seção de teclas
 const keys = document.querySelectorAll('.key');
 const checkbox = document.querySelector('.checkbox__keys');
 const switcher = document.querySelector('.switcher');
 const keysSection = document.querySelector('.piano__keys');
 
+const audioMap = {}; // Objeto que armazena os áudios das notas musicais
+const keyStatus = {}; // Objeto que rastreia o status das teclas (pressionadas ou não)
+
+// Função para reproduzir o som da nota
 const playNote = (note) => {
-    const audio = new Audio(`notes/${note}.wav`);
-    audio.play();
-}
-
-const handleMouseDown = (key) => {
-    playNote(key.getAttribute('data-note'));
-
-    if (key.className.includes('black')) {
-        key.classList.add('black--pressed');
-        return;
+// Verifica se há um áudio associado à nota
+    if (audioMap[note]) {
+ // Clona o áudio para permitir a reprodução múltipla
+        const audio = audioMap[note].cloneNode();
+        audio.play();// Reproduz o som da nota
     }
-
-    key.style.background = '#ddd';
 }
+// Manipulador para eventos de pressionar o mouse sobre uma tecla do piano
+const handleMouseDown = (key) => {
+    const note = key.getAttribute('data-note');// Obtém a nota associada à tecla
 
+    
+// Verifica se a tecla não está sendo pressionada para evitar repetições
+    if (!keyStatus[note]) {
+        playNote(note);// Reproduz o som da nota
+// Adiciona classes de estilo às teclas pretas quando pressionadas
+        if (key.className.includes('black')) {
+            key.classList.add('black--pressed');
+        } else {
+            key.style.background = 'aliceblue';// Altera o estilo de fundo das teclas brancas quando pressionadas
+        }
+
+        // Marca a tecla como pressionada
+        keyStatus[note] = true;
+    }
+}
+// Manipulador para eventos de soltar o mouse de uma tecla do piano
 const handleMouseUp = (key) => {
-
+    const note = key.getAttribute('data-note');// Obtém a nota associada à tecla
+// Remove classes de estilo das teclas pretas quando soltas
     if (key.className.includes('black')) {
         key.classList.remove('black--pressed');
-        return;
+    } else {
+        key.style.background = 'white';// Restaura o estilo de fundo das teclas brancas quando soltas
     }
 
-    key.style.background = 'white';
+    // Marca a tecla como não pressionada
+    keyStatus[note] = false;
 }
-
+// Itera sobre todas as teclas e adiciona ouvintes de eventos de clique do mouse
 keys.forEach((key) => {
-    key.addEventListener('mousedown', () => handleMouseDown(key))
-    key.addEventListener('mouseup', () => handleMouseUp(key))
+    const note = key.getAttribute('data-note');// Obtém a nota associada à tecla
+    audioMap[note] = new Audio(`notes/${note}.wav`);// Cria e armazena o áudio para a nota
+// Adiciona um ouvinte de evento de clique do mouse para detectar pressionamentos
+    key.addEventListener('mousedown', () => {
+        if (!keyStatus[note]) {
+            handleMouseDown(key);
+        }
+    });
+// Adiciona um ouvinte de evento de soltar o mouse para detectar quando a tecla é solta
+    key.addEventListener('mouseup', () => handleMouseUp(key));
 });
-
-checkbox.addEventListener('change', ({ target }) => {
-    if (target.checked) {
+// Ouvinte de evento para a caixa de verificação que controla a ativação/desativação do piano
+checkbox.addEventListener('change', (event) => {
+    const keysWithLabels = document.querySelectorAll('.key span');
+    
+    if (event.target.checked) {
         switcher.classList.add('switcher--active');
         keysSection.classList.remove('disabled-keys');
-        return;
+        
+        // Mostra os valores das teclas
+        keysWithLabels.forEach((label) => {
+            label.style.opacity = '0';
+        });
+    } else {
+        switcher.classList.remove('switcher--active');
+        keysSection.classList.add('disabled-keys');
+        
+        // Oculta os valores das teclas
+        keysWithLabels.forEach((label) => {
+            label.style.opacity = '1';
+        });
     }
-
-    switcher.classList.remove('switcher--active');
-    keysSection.classList.add('disabled-keys');
 });
 
+
+// Mapeamento de teclas do teclado para as teclas do piano
 const keyDownMapper = {
     "Tab": () => handleMouseDown(keys[0]),
     "1": () => handleMouseDown(keys[1]),
@@ -71,7 +112,7 @@ const keyDownMapper = {
     "Backspace": () => handleMouseDown(keys[22]),
     "\\": () => handleMouseDown(keys[23]),
 }
-
+// Mapeamento de teclas do teclado para eventos de soltar as teclas do piano
 const keyUpMapper = {
     "Tab": () => handleMouseUp(keys[0]),
     "1": () => handleMouseUp(keys[1]),
@@ -98,13 +139,23 @@ const keyUpMapper = {
     "Backspace": () => handleMouseUp(keys[22]),
     "\\": () => handleMouseUp(keys[23]),
 }
-
+// Ouvinte de evento para teclas do teclado pressionadas
 document.addEventListener('keydown', (event) => {
-    event.preventDefault();
-    keyDownMapper[event.key]()
-   
+    event.preventDefault();// Impede o comportamento padrão das teclas
+    const key = event.key;// Obtém a tecla pressionada
+
+     // Verifica se a tecla tem um mapeamento e não está sendo pressionada para evitar repetições
+    if (keyDownMapper[key] && !keyStatus[key]) {
+        keyDownMapper[key]();// Chama a função associada à tecla
+    }
+});
+// Ouvinte de evento para teclas do teclado soltas
+document.addEventListener('keyup', (event) => {
+    const key = event.key; // Obtém a tecla solta
+
+    // Verifica se a tecla tem um mapeamento
+    if (keyUpMapper[key]) {
+        keyUpMapper[key]();// Chama a função associada à tecla
+    }
 });
 
-document.addEventListener('keyup', (event) => {
-    keyUpMapper[event.key]()
-});
