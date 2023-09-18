c299749f733fea813822cfd230a63e049450b881
coutinhogabriel <gabriel.coutin@hotmail.com>
2023-09-18 19:38:02 -0300

    Finalização

--- README.md
+++ README.md
@@ -36,3 +36,73 @@ As teclas do piano são estilizadas com cores brancas e pretas, bordas arredonda
 Conclusão:
 
 O projeto do piano virtual é uma implementação interativa e estilizada de um piano musical. Ele oferece funcionalidades para tocar notas musicais, alternar a exibição dos nomes das teclas e tem um design que se assemelha a um piano real. Este projeto pode ser usado como uma ferramenta educacional para aprender a tocar músicas no piano ou simplesmente para diversão e entretenimento musical.
+
+
+Relatório: Projeto de Contador Regressivo
+
+1. Introdução:
+
+O projeto consiste em um contador regressivo desenvolvido em HTML, CSS e JavaScript. O contador permite definir um tempo em minutos e segundos, iniciar, pausar, reiniciar, zerar e inverter a contagem. A interface é construída utilizando o framework Bootstrap para melhor organização e estilização.
+
+2. Estrutura e Funcionalidades:
+
+O projeto é estruturado em três principais partes: o HTML define a estrutura da página, o CSS cuida do estilo e layout, e o JavaScript gerencia a lógica e interatividade.
+
+    HTML:
+        Barra de Navegação: Implementada com o framework Bootstrap, fornece navegação para diferentes seções da página.
+        Título: Exibe o título "Contador regressivo".
+        Relógio: Apresenta três colunas (minutos, segundos, milissegundos) para a contagem regressiva.
+        Botões e Inputs: Para iniciar, parar, reiniciar, zerar e inverter a contagem, além de definir o tempo por meio de inputs.
+
+    CSS:
+        Define o estilo da página, incluindo cores de fundo, fontes, tamanhos, e posicionamento dos elementos. Utiliza o Bootstrap para estilização adicional.
+
+    JavaScript:
+        Funcionalidades Principais:
+            Definir Tempo: Permite o usuário definir o tempo em minutos e segundos e iniciar a contagem regressiva.
+            Iniciar Contagem: Inicia a contagem regressiva com base no tempo definido.
+            Pausar Contagem: Pausa a contagem, permitindo sua retomada posterior.
+            Reiniciar Contagem: Reinicia a contagem a partir do tempo inicialmente definido.
+            Zerar Contagem: Zera o contador, resetando-o para zero.
+            Inverter Contagem: Inverte a contagem, permitindo uma contagem progressiva ou regressiva.
+        Interação com o DOM:
+            Manipula os elementos HTML para exibir e atualizar a contagem, habilitar/desabilitar botões e interagir com o usuário.
+        Modal para Definir Tempo:
+            Utiliza um modal para permitir que o usuário defina o tempo inicial antes de iniciar a contagem.
+
+3. Conclusão:
+
+O projeto de contador regressivo oferece uma interface intuitiva para definir e controlar uma contagem regressiva. Permite uma experiência interativa ao usuário, facilitando a gestão do tempo e tornando-se útil em várias situações, como contagem para atividades, prazos, entre outros. A estrutura bem organizada, o estilo atraente e as funcionalidades bem implementadas contribuem para uma aplicação eficaz e de fácil utilização.
+
+Relatório do Projeto: Jogo da velha
+
+Introdução 
+O script JavaScript fornecido implementa a lógica do jogo da velha para a página web. Ele define os jogadores, controla a lógica do jogo e verifica se houve um vencedor ou se o jogo terminou em empate.
+
+Estrutura Geral
+Inicialização da Página
+window.onload: Esta função é executada quando a página é completamente carregada. Neste caso, ela esconde a div com o id game.
+Definição de Jogadores
+Jogador(nome, forma): Esta função é um construtor para criar objetos de jogador. Cada jogador tem um nome (nome) e uma forma (forma) que será usada para marcar as células do tabuleiro (X ou O).
+Variáveis Globais
+jogador1, jogador2: Representam os jogadores.
+jogadorAtual: Indica qual jogador está atualmente em sua rodada.
+formas: Um array contendo as formas que os jogadores usam para marcar as células (X e O).
+index: Variável não utilizada no script, permanece como null.
+Tabuleiro
+tabuleiro: Um array de 9 elementos que representa o estado do tabuleiro. As posições preenchidas por X ou O são armazenadas aqui.
+Inicialização do Jogo
+initGame(): Esta função é chamada quando o botão "Iniciar" é clicado. Ela obtém os nomes dos jogadores do formulário, cria os objetos de jogador e inicia o jogo, tornando a div com o id game visível.
+Funções de Verificação
+tabuleiroIsFilled(): Verifica se todas as células do tabuleiro estão preenchidas. Retorna verdadeiro se o tabuleiro estiver completamente preenchido.
+allElementsInSomeLine(): Verifica se algum jogador ganhou em alguma linha horizontal.
+allElementsInSomeColumn(): Verifica se algum jogador ganhou em alguma coluna vertical.
+allElementsInSomeDiagonal(): Verifica se algum jogador ganhou em alguma diagonal.
+Atualização do Jogador Atual
+setLabelJogadorAtual(): Atualiza o texto que indica qual jogador está na rodada atual.
+Marcação de Células do Tabuleiro
+setOnCeil(cel, pos): Esta função é chamada quando uma célula do tabuleiro é clicada. Ela verifica se a célula já está marcada, caso contrário, marca com a forma do jogador atual. Em seguida, atualiza o jogador da rodada e chama as funções de verificação de vencedores.
+Reiniciar o Jogo
+reset(): Esta função é chamada quando o jogo precisa ser reiniciado. Ela recarrega a página, reiniciando todo o estado do jogo.
+Conclusão
+O script JavaScript fornece a lógica necessária para a implementação de um jogo da velha funcional. Ele controla a interação do usuário, verifica o estado do tabuleiro e determina se houve um vencedor ou se o jogo terminou em empate. Além disso, permite a reinicialização do jogo quando necessário. Recomenda-se testar o script em conjunto com a estrutura HTML fornecida para uma experiência completa do jogo.
---
+++ scriptC.js
@@ -0,0 +1,192 @@
+// Definição das variáveis e elementos utilizados
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+const botaoParar = document.getElementById('pararContador');
+const botaoReiniciar = document.getElementById('reiniciar');
+const min = document.getElementById('minutosInput');
+const seg = document.getElementById('segundosInput');
+const botaoiniciar = document.getElementById('iniciar');
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fundo = document.querySelector("#fade");
+
+
+let intervalo = 10; // Intervalo em milissegundos (10 milissegundos)
+let Contador;
+let pausado = false;
+let tempoRestante = 0;
+let tempReinicio=0;
+let tempInvert=0;
+
+
+
+
+
+
+// Função para abrir e fechar o modal
+
+const mudarModal = () => {
+  modal.classList.toggle("hide");
+  fundo.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fundo].forEach((el) => {
+  el.addEventListener("click", () => mudarModal());
+});
+
+// Função para definir o tempo e iniciar o contado
+function definirCont() {
+   
+   
+//calculo para contar o tempo do relogio
+    tempoRestante = ((min.value * 60000) + (seg.value * 1000));
+    tempReinicio = ((min.value * 60000) + (seg.value * 1000));
+    tempInvert = tempoRestante;
+    botaoiniciar.style.display = "inline";
+    openModalButton.textContent = "Redefinir"
+    mudarModal();
+    clearInterval(Contador);
+    iniciarContador();
+}
+
+// Função para zerar o contador
+function Zerar() {
+    clearInterval(Contador);
+    tempoRestante = 0;
+    minutos.textContent = '00';
+    segundos.textContent = '00';
+    milisegundos.textContent = '00';
+    botaoParar.textContent = 'Parar Contagem';
+    botaoParar.disabled = true;
+    pausado = false;
+    botaoReiniciar.disabled = false;
+}
+
+
+// Função para reiniciar o contador
+function reiniciar() {
+    clearInterval(Contador);
+    tempoRestante = tempReinicio; // Restaura o valor original
+    botaoParar.textContent = 'Parar Contagem';
+    botaoParar.disabled = false;
+    pausado = false;
+    botaoReiniciar.disabled = true;
+    
+
+    Contador = setInterval(function () {
+        if (!pausado) {
+            if (tempoRestante <= 0) {
+                clearInterval(Contador);
+                minutos.textContent = '00';
+                segundos.textContent = '00';
+                milisegundos.textContent = '00';
+                botaoParar.disabled = true;
+            } else {
+                tempoRestante -= intervalo;
+                let minutosr = Math.floor(tempoRestante / 60000);
+                let segundosr = Math.floor((tempoRestante % 60000) / 1000);
+                let milisegundosr = tempoRestante % 1000;
+
+                minutos.textContent = minutosr < 10 ? '0' + minutosr : minutosr;
+                segundos.textContent = segundosr < 10 ? '0' + segundosr : segundosr;
+                milisegundos.textContent = milisegundosr < 10 ? '00' + milisegundosr : (milisegundosr < 100 ? '0' + milisegundosr : milisegundosr);
+            }
+        }
+    }, intervalo);
+}
+
+
+// Função para inverter a contagem
+function inverter() {
+    if (!pausado) {
+        clearInterval(Contador);
+        pausado = true;
+        botaoParar.textContent = 'Retomar Contagem';
+    } else {
+        pausado = false;
+        botaoParar.textContent = 'Parar Contagem';
+        Contador = setInterval(function () {
+            if (!pausado) {
+                if (tempoRestante < 0) {
+                    clearInterval(Contador);
+                    minutos.textContent = '00';
+                    segundos.textContent = '00';
+                    milisegundos.textContent = '00';
+                    botaoParar.disabled = true;
+                } else {
+        
+                
+                    if(tempInvert>=tempoRestante){
+                    tempoRestante += intervalo;
+                    let minutosI = Math.floor(tempoRestante / 60000);
+                    let segundosI = Math.floor((tempoRestante % 60000) / 1000);
+                    let milisegundosI = tempoRestante % 1000;
+    
+                    minutos.textContent = minutosI < 10 ? '0' + minutosI : minutosI;
+                    segundos.textContent = segundosI < 10 ? '0' + segundosI : segundosI;
+                    milisegundos.textContent = milisegundosI < 10 ? '00' + milisegundosI : (milisegundosI < 100 ? '0' + milisegundosI : milisegundosI);}
+                }
+            }
+        }, intervalo);
+    }
+}
+
+
+
+botaoParar.addEventListener('click', function () {
+    if (!pausado) {
+        clearInterval(Contador);
+        pausado = true;
+        botaoParar.textContent = 'Retomar Contagem';
+        botaoReiniciar.disabled = false;
+
+
+    } else {
+       
+        iniciarContador();
+
+
+
+    }
+
+});
+
+
+// Função para iniciar o contador
+function iniciarContador() {
+    
+    botaoParar.textContent = 'Parar Contagem';
+    botaoParar.disabled = false;
+    pausado = false;
+    botaoReiniciar.disabled = true;
+    
+ 
+
+    Contador = setInterval(function () {
+       
+        if (!pausado) {
+            if (tempoRestante <= 0) {
+                clearInterval(Contador);
+                minutos.textContent = '00';
+                segundos.textContent = '00';
+                milisegundos.textContent = '00';
+                botaoParar.disabled = true;
+
+            } else {
+
+                tempoRestante -= intervalo;
+                let minutosI = Math.floor(tempoRestante / 60000);
+                let segundosI = Math.floor((tempoRestante % 60000) / 1000);
+                let milisegundosI = tempoRestante % 1000;
+
+                minutos.textContent = minutosI < 10 ? '0' + minutosI : minutosI;
+                segundos.textContent = segundosI < 10 ? '0' + segundosI : segundosI;
+                milisegundos.textContent = milisegundosI < 10 ? '00' + milisegundosI : (milisegundosI < 100 ? '0' + milisegundosI : milisegundosI);
+            }
+        }
+    }, intervalo);
+}
+
+
+
